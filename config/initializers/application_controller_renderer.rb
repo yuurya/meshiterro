e52b9533cbc9a6ec008aae5291a0fc472d8e8054
yuurya <yuki_pooh1990@icloud.com>
2021-01-29 14:30:42 +0900

    app5勝終了時

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '48e89ff3c30bfaf700d908ab88c91cbe5661ed4171390cef07ad22c03e17fecb896f8c1fdd70364c0013e21eb7bb7b662eac2b8af2920dc198a9b02146966fea'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'post_images#index'
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
